570bea28cda9465f590a99271db50aaa68bb328f
Paul Jung <pbnjcub@gmail.com>
2022-07-20 09:48:06 -0400

    commit tree 8c9b4e9443d6cf28802675340219655124e84a51

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
 const init = () => {
+    const inputForm = document.querySelector('form');
+    inputForm.addEventListener('submit', (event) => {
+        event.preventDefault()
+        const input = document.querySelector('input#searchByID')
+        
+        console.log(input.value)
+
+        fetch('http://localhost:3000/movies')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+        })
+
+    })
   
 }
 
